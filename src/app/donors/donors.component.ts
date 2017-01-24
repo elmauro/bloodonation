f03bc7f49889bec57cebe32dc0065c7c715e35c9
Mauricio Cadavid <mauricio.cadavid@yuxipacific.com>
2017-01-23 23:54:05 -0500

    adding maps

--- src/app/donors/donors.component.ts
+++ src/app/donors/donors.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Donor } from './donor';
 import { DonorsService } from '../donors.service';
 
 import { EsriLoaderService } from 'angular2-esri-loader';
-import { Esri4MapComponent } from 'angular2-esri4-components';
 
 @Component({
   selector: 'app-donors',
   templateUrl: './donors.component.html',
-  styleUrls: ['./donors.component.css'],
-  providers: [EsriLoaderService]
+  styleUrls: ['./donors.component.css']
 })
 
 export class DonorsComponent implements OnInit {
+
+  @ViewChild('map') mapEl: ElementRef;
+
+  map: any;
+  view: any;
+  track: any;
 
   lat = 43.07493;
   lng = -89.381388;
@@ -26,19 +30,6 @@ export class DonorsComponent implements OnInit {
   connection;
   message;
 
-  mapProperties: __esri.MapProperties = {
-    basemap: 'dark-gray'
-  };
-  mapViewProperties: __esri.MapViewProperties = {
-    center: [-118, 34.5],
-    zoom: 8
-  };
-  map: __esri.Map;
-  mapView: __esri.MapView;
-  track: __esri.Track;
-
-  @ViewChild(Esri4MapComponent) esriComponent: Esri4MapComponent;
-  
   constructor(
     private router: Router,
     private donorsService: DonorsService,
@@ -55,11 +46,48 @@ export class DonorsComponent implements OnInit {
     this.connection = this.donorsService.getMessages().subscribe(message => {
       this.onGetAllDonors();
     })
-  }
 
-  onMapInit(mapInfo: {map: __esri.Map, mapView: __esri.MapView}) {
-    this.map = mapInfo.map;
-    this.mapView = mapInfo.mapView;
+    // only load the ArcGIS API for JavaScript when this component is loaded
+    return this.esriLoader.load({
+      // use a specific version of the API instead of the latest
+      url: '//js.arcgis.com/4.2/'
+    }).then(() => {
+      // load the map class needed to create a new map
+      this.esriLoader.loadModules(['esri/widgets/Track','esri/Map','esri/views/MapView','dojo/domReady!']).then(([Track,Map,MapView]) => {
+
+        this.map = new Map({
+          basemap: "dark-gray"
+        });
+
+        // Create the MapView
+          this.view = new MapView({
+          container: this.mapEl.nativeElement,
+          map: this.map,
+          center: [-116.3031, 43.6088],
+          zoom: 12
+        });
+
+        this.track = new Track({
+          view: this.view
+        });
+        
+        this.view.ui.add(this.track, "top-left");
+
+        this.view.on("click", function(event) {
+            // Get the coordinates of the click on the view
+            // around the decimals to 3 decimals
+            var lat = Math.round(event.mapPoint.latitude * 1000) / 1000;
+            var lon = Math.round(event.mapPoint.longitude * 1000) / 1000;
+
+            console.log(lat, lon);
+        }); 
+
+        this.view.then(function() {
+          this.track.start();
+        });
+
+      });
+    });
   }
 
   onSelect(donor: Donor): void {
